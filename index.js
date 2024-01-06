faad921716f81f4c4dd49a22560dd101e05fb688
lubiano83 <lubiano83@gmail.com>
2024-01-06 18:55:19 -0300

    agregando productos al carro de compras

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 // Definiendo las Propiedades
 class Producto {
-    constructor(codigo, imagen, tipo, marca, modelo, medida, otro, cantidad, precio,) {
+    constructor(codigo, imagen, tipo, marca, modelo, otro1, otro2, cantidad, precio,) {
         this.codigo = codigo;
         this.imagen = imagen;
         this.tipo = tipo;
         this.marca = marca;
         this.modelo = modelo;
-        this.medida = medida;
-        this.otro = otro;
+        this.otro1 = otro1;
+        this.otro2 = otro2;
         this.cantidad = cantidad;
         this.precio = precio;
     }
 }
 
 // Neumaticos
-const PROD1 = new Producto("50170-0", "./img/hankook K715.jpeg","NEUMATICOS", "HANKOOK", "K715", "155/65R13", "","4", 59890);
-const PROD2 = new Producto("50174-3", "./img/hankook H724.jpeg","NEUMATICOS", "HANKOOK", "H724", "185/65R14", "","4", 59890);
-const PROD3 = new Producto("50595-1", "./img/hankook H308.jpeg","NEUMATICOS", "HANKOOK", "H308", "175/60R15", "","4", 49890);
+const PROD1 = new Producto("50170-0", "./img/hankook K715.jpeg", "NEUMATICOS", "HANKOOK", "K715", "155/65R13", "OPTIMO", "4", 59890);
+const PROD2 = new Producto("50174-3", "./img/hankook H724.jpeg", "NEUMATICOS", "HANKOOK", "H724", "185/65R14", "OPTIMO", "4", 59890);
+const PROD3 = new Producto("50595-1", "./img/hankook H308.jpeg", "NEUMATICOS", "HANKOOK", "H308", "175/60R15", "KINERGY", "4", 49890);
 // Baterias
-const PROD4 = new Producto("09076-K", "./img/hankook MF54321.jpeg","BATERIAS", "HANKOOK", "MF54321", "45AH", "450CCA","4", 69890);
-const PROD5 = new Producto("09030-1", "./img/hankook MF55457.jpeg","BATERIAS", "HANKOOK", "MF55457", "55AH", "480CCA","7", 79890);
-const PROD6 = new Producto("09095-6", "./img/hankook MF47600.jpeg","BATERIAS", "HANKOOK", "MF47600", "60AH", "600CCA","3", 98980);
+const PROD4 = new Producto("09076-K", "./img/hankook MF54321.jpeg", "BATERIAS", "HANKOOK", "MF54321", "45AH", "450CCA", "4", 69890);
+const PROD5 = new Producto("09030-1", "./img/hankook MF55457.jpeg", "BATERIAS", "HANKOOK", "MF55457", "55AH", "480CCA", "7", 79890);
+const PROD6 = new Producto("09095-6", "./img/hankook MF47600.jpeg", "BATERIAS", "HANKOOK", "MF47600", "60AH", "600CCA", "3", 98980);
 // Aceites
-const PROD7 = new Producto("22212-7", "./img/shell 5w30 hx8.webp","ACEITES", "SHELL", "HX8", "5W-30", "BENCINERO","6", 47290);
-const PROD8 = new Producto("22135-K", "./img/shell 15w40 hx5.webp","ACEITES", "SHELL", "HX5", "15W-40", "BENCINERO","6", 29690);
-const PROD9 = new Producto("22013-2", "./img/10w-40 petroleo shell.jpeg","ACEITES", "SHELL", "R5", "10W-40", "PETROLERO","3", 35990);
+const PROD7 = new Producto("22212-7", "./img/shell 5w30 hx8.webp", "ACEITES", "SHELL", "HX8", "5W-30", "BENCINERO", "6", 47290);
+const PROD8 = new Producto("22135-K", "./img/shell 15w40 hx5.webp", "ACEITES", "SHELL", "HX5", "15W-40", "BENCINERO", "6", 29690);
+const PROD9 = new Producto("22013-2", "./img/10w-40 petroleo shell.jpeg", "ACEITES", "SHELL", "R5", "10W-40", "PETROLERO", "3", 35990);
 // Filtros
-const PROD10 = new Producto("25508-4", "./img/man c30171.jpeg","FILTROS", "MANN", "C30171", "", "FILTRO AIRE","2", 9900);
-const PROD11 = new Producto("25479-7", "./img/mann hu 718:5X.jpeg","FILTROS", "MANN", "HU718/5X", "", "FILTRO ELEMENTO","2", 9700);
-const PROD12 = new Producto("26007-K", "./img/mann w610:6.jpeg","FILTROS", "MANN", "W610/6", "", "FILTRO ACEITE","2", 6280);
+const PROD10 = new Producto("25508-4", "./img/man c30171.jpeg", "FILTROS", "MANN", "C30171", "", "FILTRO AIRE", "2", 9900);
+const PROD11 = new Producto("25479-7", "./img/mann hu 718:5X.jpeg", "FILTROS", "MANN", "HU718/5X", "", "FILTRO ELEMENTO", "2", 9700);
+const PROD12 = new Producto("26007-K", "./img/mann w610:6.jpeg", "FILTROS", "MANN", "W610/6", "", "FILTRO ACEITE", "2", 6280);
 // Aromatizantes
-const PROD13 = new Producto("42088-3", "./img/paloma parfum.png","AROMATIZANTES", "PALOMA", "PARFUM", "", "VAINILLA","20", 1800);
-const PROD14 = new Producto("42055-7", "./img/paloma aqua balls.png","AROMATIZANTES", "PALOMA", "WOODY", "", "FLORAL","2", 2400);
-const PROD15 = new Producto("42120-0", "./img/paloma happy bag.png","AROMATIZANTES", "PALOMA", "HAPPY BAG", "", "SPORT","10", 1890);
+const PROD13 = new Producto("42088-3", "./img/paloma parfum.png", "AROMATIZANTES", "PALOMA", "PARFUM", "", "VAINILLA", "20", 1800);
+const PROD14 = new Producto("42055-7", "./img/paloma aqua balls.png", "AROMATIZANTES", "PALOMA", "WOODY", "", "FLORAL", "2", 2400);
+const PROD15 = new Producto("42120-0", "./img/paloma happy bag.png", "AROMATIZANTES", "PALOMA", "HAPPY BAG", "", "SPORT", "10", 1890);
 
 // Contenedor Productos
 const PRODUCTOS_CONTENEDOR = document.getElementById("productosContenedor");
@@ -40,29 +40,28 @@ const PRODUCTOS = [PROD1, PROD2, PROD3, PROD4, PROD5, PROD6, PROD7, PROD8, PROD9
 // Filtros Nav
 const BOTON_NAV = document.querySelectorAll(".dropdown-item");
 const TITULO_PRINCIPAL = document.getElementById("tituloPrincipal");
-// Boton Filtrar
-const BTN = document.getElementById("btn"); // document. es un objeto y getElementById es un metodo.
+// Botones
 let botonesAgregar = document.querySelectorAll(".agregar");
 // Carrito
 const TOGGLE_CARRITO = document.getElementById('toggleCarrito');
 const CARRITO = document.querySelector('.carrito');
 const NUMERITO = document.querySelector("#numerito");
+// Carrito de Compras
+const PRODUCTOS_EN_CARRITO = [];
+const CARRITO_TOTAL = document.getElementById("carritoTotal");
 
 // Cargar Productos
-function cargarProductos (productosElegidos){
+function cargarProductos(productosElegidos) {
     PRODUCTOS_CONTENEDOR.innerHTML = ""; // Limpiar el contenedor antes de agregar nuevos productos
     productosElegidos.forEach(contenedor__box => {
         const ARTICLE = document.createElement("article");
         ARTICLE.className = "contenedor__box";
         ARTICLE.innerHTML = `
             <div class="box__foto">
-                <img src="${contenedor__box.imagen}" alt="${contenedor__box.tipo},${contenedor__box.marca},${contenedor__box.modelo},${contenedor__box.medida},${contenedor__box.otro}" class="foto">
+                <img src="${contenedor__box.imagen}" alt= "autoshop,${contenedor__box.tipo},${contenedor__box.marca},${contenedor__box.modelo},${contenedor__box.otro1},${contenedor__box.otro2}" class="foto">
             </div>
             <div class="box__texto">
-                <h4>Marca: ${contenedor__box.marca}</h4>
-                <h4>Modelo: ${contenedor__box.modelo}</h4>
-                <h4>Medida: ${contenedor__box.medida || "-----"}</h4>
-                <h4>Otro: ${contenedor__box.otro || "-----"}</h4>
+                <h4>Descripción: ${contenedor__box.marca}, ${contenedor__box.modelo}, ${contenedor__box.otro1}, ${contenedor__box.otro2}</h4>
                 <h4>Precio: $${formatearPrecio(contenedor__box.precio)}</h4>
                 <h4>Cantidad: ${contenedor__box.cantidad}</h4>
                 <button class="agregar" id="${contenedor__box.codigo}">Agregar</button>
@@ -94,7 +93,7 @@ BOTON_NAV.forEach(boton => {
         if (e.currentTarget.id !== "TODOS") {
             const PRODUCTO_CATEGORIA = PRODUCTOS.find(contenedor__box => contenedor__box.tipo === e.currentTarget.id);
             TITULO_PRINCIPAL.innerText = capitalizarPrimeraLetra(PRODUCTO_CATEGORIA.tipo);
-    
+
             const PRODUCTOS_FILTRADOS = PRODUCTOS.filter(contenedor__box => contenedor__box.tipo === e.currentTarget.id);
             cargarProductos(PRODUCTOS_FILTRADOS);
         } else {
@@ -109,15 +108,15 @@ function removerAcentos(texto) { // Función para remover acentos y caracteres e
     return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
 }
 const BARRA_BUSQUEDA = document.getElementById("barraBusqueda");
-BARRA_BUSQUEDA.addEventListener('input', function() {
+BARRA_BUSQUEDA.addEventListener('input', function () {
     const TEXTO_BUSQUEDA = removerAcentos(BARRA_BUSQUEDA.value.trim()).toLowerCase();
 
     const PRODUCTOS_FILTRADOS = PRODUCTOS.filter(contenedor__box => {
         const TIPO_NORMALIZED = removerAcentos(contenedor__box.tipo);
         const MARCA_NORMALIZED = removerAcentos(contenedor__box.marca);
         const MODELO_NORMALIZED = removerAcentos(contenedor__box.modelo);
-        const MEDIDA_NORMALIZED = removerAcentos(contenedor__box.medida);
-        const OTRO_NORMALIZED = removerAcentos(contenedor__box.otro);
+        const MEDIDA_NORMALIZED = removerAcentos(contenedor__box.otro1);
+        const OTRO_NORMALIZED = removerAcentos(contenedor__box.otro2);
 
         return (TIPO_NORMALIZED.includes(TEXTO_BUSQUEDA) || MARCA_NORMALIZED.includes(TEXTO_BUSQUEDA) || MODELO_NORMALIZED.includes(TEXTO_BUSQUEDA) || MEDIDA_NORMALIZED.includes(TEXTO_BUSQUEDA) || OTRO_NORMALIZED.includes(TEXTO_BUSQUEDA));
     });
@@ -136,34 +135,99 @@ BARRA_BUSQUEDA.addEventListener('input', function() {
 toggleCarrito.addEventListener('click', () => {
     CARRITO.classList.toggle('mostrar-carrito'); // Usa toggle para alternar la clase
 });
-// Carrito de Compras
-const PRODUCTOS_EN_CARRITO = [];
+
 // Botones Agregar
 function actualizarBotonesAgregar() {
     botonesAgregar = document.querySelectorAll(".agregar");
-    botonesAgregar.forEach(boton =>{
+    botonesAgregar.forEach(boton => {
         boton.addEventListener("click", agregarAlCarrito);
     })
 }
- // Agregar al Carrito
+
+// Agregar al Carrito
 function agregarAlCarrito(e) {
     const CODIGO_BOTON = e.currentTarget.id;
-    const PRODUCTO_AGREGADO = PRODUCTOS.find(producto => producto.codigo === CODIGO_BOTON);
+    const PRODUCTO_AGREGADO = PRODUCTOS.find(contenedor__box => contenedor__box.codigo === CODIGO_BOTON);
 
-    if(PRODUCTOS_EN_CARRITO.some(producto => producto.codigo === CODIGO_BOTON)){
-        const INDEX = PRODUCTOS_EN_CARRITO.findIndex(producto => producto.codigo === CODIGO_BOTON);
+    if (PRODUCTOS_EN_CARRITO.some(contenedor__box => contenedor__box.codigo === CODIGO_BOTON)) {
+        const INDEX = PRODUCTOS_EN_CARRITO.findIndex(contenedor__box => contenedor__box.codigo === CODIGO_BOTON);
         PRODUCTOS_EN_CARRITO[INDEX].cantidad++
     } else {
         PRODUCTO_AGREGADO.cantidad = 1;
         PRODUCTOS_EN_CARRITO.push(PRODUCTO_AGREGADO);
     }
-    actualizarNumerito()
-}
-// Actulizar Numerito de Carrito
+    cargarCarrito(PRODUCTOS_EN_CARRITO);
+    actualizarNumerito();
+    localStorage.setItem("PRODUCTOS_EN_CARRITO", JSON.stringify(PRODUCTOS_EN_CARRITO)); // almacenar el carrito en local storage
+};
+document.addEventListener('DOMContentLoaded', () => { // para que no se vacie el carro cuando se refresca y se agrega un nuevo producto
+    let productosGuardados = JSON.parse(localStorage.getItem("PRODUCTOS_EN_CARRITO")) || [];
+    PRODUCTOS_EN_CARRITO.push(...productosGuardados);
+    cargarCarrito(productosGuardados);
+});
+
+// Actualizar Numerito de Carrito
 function actualizarNumerito() {
-    let nuevoNumerito = PRODUCTOS_EN_CARRITO.reduce((acc, producto) => acc + producto.cantidad, 0);
+    let nuevoNumerito = PRODUCTOS_EN_CARRITO.reduce((acc, contenedor__box) => acc + contenedor__box.cantidad, 0);
     NUMERITO.innerText = nuevoNumerito;
+    localStorage.setItem("nuevoNumerito", nuevoNumerito); // Guardar en localStorage
 }
+document.addEventListener('DOMContentLoaded', () => { // para que no se vacie el numerito cuando se refresca y se agrega un nuevo producto
+    let nuevoNumeritoGuardado = localStorage.getItem("nuevoNumerito");
+    if (nuevoNumeritoGuardado !== null) {
+        NUMERITO.innerText = nuevoNumeritoGuardado;
+    }
+});
+
+function cargarCarrito(productosElegidos) {
+    const CARRITO_CONTENEDOR = document.getElementById("carritoContenedor");
+
+    CARRITO_CONTENEDOR.innerHTML = ""; // Limpiar el contenedor antes de agregar nuevos productos
+
+    productosElegidos.forEach(contenedor__box => {
+        const ARTICLE = document.createElement("article");
+        ARTICLE.className = "contenedor__box";
+        ARTICLE.innerHTML = `
+            <div class="box__foto">
+                <img src="${contenedor__box.imagen}" alt="autoshop, ${contenedor__box.tipo}, ${contenedor__box.marca}, ${contenedor__box.modelo}, ${contenedor__box.medida}, ${contenedor__box.otro}" class="foto">
+            </div>
+            <div class="box__texto">
+                <h4>Descripción: ${contenedor__box.marca} ${contenedor__box.modelo} ${contenedor__box.medida} ${contenedor__box.otro}</h4>
+                <h4>Precio: $${formatearPrecio(contenedor__box.precio)}</h4>
+                <h4>Cantidad: ${contenedor__box.cantidad}</h4>
+            </div>
+            <div class="box__eliminar">
+                <button class="botonEliminar" id="${contenedor__box.codigo}">Eliminar</button>
+            </div>
+        `;
+        CARRITO_CONTENEDOR.appendChild(ARTICLE); // Agregar el elemento ARTICLE al contenedor
+    });
+}
+const PRODUCTOS_GUARDADOS = JSON.parse(localStorage.getItem("PRODUCTOS_EN_CARRITO")) || []; // Llamar a cargarCarrito con los productos guardados en localStorage al iniciar
+cargarCarrito(PRODUCTOS_GUARDADOS);
+
+// Boton Eliminar dle Carrito
+function eliminarDelCarrito(e) {
+    const BOTON_ELIMINAR = e.currentTarget.codigo;
+    
+    // Encuentra el índice del producto en el carrito
+    const INDEX = PRODUCTOS_EN_CARRITO.findIndex(contenedor__box => contenedor__box.codigo === BOTON_ELIMINAR);
+    
+    if (INDEX !== -1) {
+        // Reduce la cantidad o elimina el producto del carrito según corresponda
+        if (PRODUCTOS_EN_CARRITO[INDEX].cantidad > 1) {
+            PRODUCTOS_EN_CARRITO[INDEX].cantidad--;
+        } else {
+            PRODUCTOS_EN_CARRITO.splice(INDEX, 1); // Elimina el producto del array si la cantidad es 1
+        }
+    }
+    
+    cargarCarrito(PRODUCTOS_EN_CARRITO);
+    actualizarNumerito();
+    localStorage.setItem("PRODUCTOS_EN_CARRITO", JSON.stringify(PRODUCTOS_EN_CARRITO)); // Actualiza localStorage
+}
+
+
 
 
 
